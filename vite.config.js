cb29f704f643f94d55ceaa7dda4161e384487a44
John Thomas Alog <johnthomasalog@gmail.com>
2025-12-09 16:19:04 +0800

    update server configuration to use host '0.0.0.0' and port 8080

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: '192.168.100.82',
-    port: 5002,
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
   }
 })
